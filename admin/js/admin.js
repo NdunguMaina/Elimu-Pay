1f52844f5045ffd27459ce92f0b17633d9e4fb29
Ndung'u Maina <ndungumaina361@gmail.com>
2022-12-30 12:58:34 +0300

    random

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,63 +1,33 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyA41TSZcwPpgIJMQu4QklY-2s8rJmHNdEw",
   authDomain: "elimu-pay.firebaseapp.com",
+  databaseURL: "https://elimu-pay-default-rtdb.firebaseio.com",
   projectId: "elimu-pay",
   storageBucket: "elimu-pay.appspot.com",
   messagingSenderId: "282414316037",
   appId: "1:282414316037:web:fe8ba8144036dc70917a6c",
   measurementId: "G-CC9HDKLT7T"
 };
-
+ 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+var db = firebase.database();
 
-import { getDatabase, ref, set, child, update, remove }
-  from "https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js";
+function save() {
+  var sName = document.getElementById("sName").value
+  var mpesa = document.getElementById("mpesa").value
+  var kcbAC = document.getElementById("kcbAC") .value
+  var equityAC = document.getElementById("equityAC").value
+  var coopAC = document.getElementById("coopAC").value
 
-const db = getDatabase();
+  // var addSchool = document.getElementById("addSchool").value
 
-// get references
+  db.ref('School/' + sName).set({
+    mpesa: mpesa,
+    kcb: kcbAC,
+    equity: equityAC,
+    coop: coopAC,
 
-var sName = document.getElementById("sName");
-var mpesa = document.getElementById("mpesa");
-var kcbAC = document.getElementById("kcbAC");
-var equityAC = document.getElementById("equityAC");
-var coopAC = document.getElementById("coopAC");
-
-var addSchool = document.getElementById("addSchool");
-
-function InsertData() {
-  set(ref(db, "School/" + sName.value),
-    {
-      "School Name": sName.value,
-      "Mpesa": mpesa.value,
-      "KCB": kcbAC.value,
-      "Equity": equityAC.value,
-      "Coop": coopAC.value
-
-    }
-  )
-    .then(() => {
-      alert("data has been stored successfully")
-    })
-    .catch((error) => {
-      alert("Unsuccessfull error" + error);
-    });
-    }
-
-function now() {
-      alert("hello")
-    }
-// assign events to buttons
-addSchool.addEventListener("click", InsertData);
-
+  })
+  alert("data has been stored successfully")
+}
